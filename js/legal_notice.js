a0bbba8b1569a81d0d28bd175955c0745089231b
Norman <normansprenger@gmail.com>
2024-07-05 20:19:00 +0200

    general update

--- js/legal_notice.js
+++ js/legal_notice.js
@@ -1,10 +1,27 @@
 async function initLegalPolicy() {
     await includeHTML(); // Sicherstellen, dass includeHTML() abgeschlossen ist
-    setTimeout(() => {
-        hideHeaderContainerRightUserIcon(); // Verzögerte Ausführung
-    }, 100); // Beispielhafte Verzögerung, die je nach Ladezeit angepasst werden muss
+    if (currentUser == null) {
+        setTimeout(() => {
+            hideHeaderContainerRightUserIcon(); // Verzögerte Ausführung
+        }, 100); // Beispielhafte Verzögerung, die je nach Ladezeit angepasst werden muss
+        setTimeout(() => {
+            hideDesktopContainer(); // Verzögerte Ausführung
+        }, 100); // Beispielhafte Verzögerung, die je nach Ladezeit angepasst werden muss
+    }
 }
 
+
 function hideHeaderContainerRightUserIcon() {
-    document.getElementById('headerContainerRightUserIcon').style='display:none;';
+    document.getElementById('headerContainerRightUserIcon').style = 'display:none;';
+}
+
+
+function hideDesktopContainer() {
+    document.getElementById('desktopContainer').style = "display: none;"
+}
+
+
+function backToLastPage() {
+    console.log('Funktion fertig stellen. Merker für letzte Seite implementieren');
+    open("../index.html", "_self");
 }
