190d6da869f47967860e428bbd4d80c2d0af50fd
Norman <normansprenger@gmail.com>
2024-07-06 18:21:07 +0200

    update CSS

--- js/remoteStorage.js
+++ js/remoteStorage.js
@@ -1,3 +1,6 @@
+let currentUser = 'test'; // der Nutzer der momentan angemeldet ist muss lokal gespeichert werden -> nicht in die Firebase
+let lastPage = null; // hier wird die URL gespeichert welche verwendet wurde bevor man auf HELP LEGAL NOTICE oder PRIVACY POLICY geht damit man von dort aus zu der Seite zurückkehren kann.
+
 const BASE_URL = 'https://remotestoragejoin-d0140-default-rtdb.europe-west1.firebasedatabase.app/';
 
 async function fetchContacts() {
