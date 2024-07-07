0474a034a613842c7860a7a8b22e94cc65489bdc
Norman <normansprenger@gmail.com>
2024-07-07 16:49:39 +0200

    flyin header  added

--- js/help.js
+++ js/help.js
@@ -27,10 +27,4 @@ function backToLastPage() {
     open("../index.html", "_self");
 }
 
-function showHeaderMenu(){
-    if(document.getElementById('headerMobileMenu').style.right == "-180px"){
-    document.getElementById('headerMobileMenu').style.right = "10px"}
-    else{
-        document.getElementById('headerMobileMenu').style.right = "-180px"
-    };
-}
+
--- js/privacy_policy.js
+++ js/privacy_policy.js
@@ -27,10 +27,3 @@ function backToLastPage() {
     open("../index.html", "_self");
 }
 
-function showHeaderMenu(){
-    if(document.getElementById('headerMobileMenu').style.right == "-180px"){
-    document.getElementById('headerMobileMenu').style.right = "10px"}
-    else{
-        document.getElementById('headerMobileMenu').style.right = "-180px"
-    };
-}
