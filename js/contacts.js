84d87172de41bb7464d1b804d613f74da51137cc
Norman <normansprenger@gmail.com>
2024-07-08 16:36:21 +0200

    update

---
+++ js/contacts.js
@@ -0,0 +1,53 @@
+async function fetchContacts() {
+    try {
+        const response = await fetch(`${BASE_URL}contacts.json`);
+        if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok');
+        }
+        const data = await response.json();
+        displayContacts(data);
+    } catch (error) {
+        console.error('Es gab ein Problem mit dem Fetch-Vorgang:', error);
+    }
+}
+
+function displayContacts(data) {
+    const contactList = document.getElementById('contactList');
+    contactList.innerHTML = '';
+
+    const contactsArray = Object.keys(data).map(key => data[key]);
+    contactsArray.sort((a, b) => a.name.localeCompare(b.name));
+
+    let currentLetter = '';
+
+    for (const contact of contactsArray) {
+        const firstLetter = contact.name.charAt(0).toUpperCase();
+
+        if (firstLetter !== currentLetter) {
+            currentLetter = firstLetter;
+
+            const letterHeader = document.createElement('div');
+            letterHeader.className = 'letterHeader';
+            letterHeader.innerHTML = `
+                <h2>${currentLetter}</h2>
+                <hr>
+            `;
+            contactList.appendChild(letterHeader);
+        }
+
+        const contactElement = document.createElement('div');
+        contactElement.className = 'contactItem';
+        contactElement.innerHTML = `
+            <div class="shortName" style="background-color: ${contact.color};">
+                <span class="initials">${contact.initials}</span> <!-- Initialen aus den Daten -->
+            </div>
+            <div class="contactDetails">
+                <h3>${contact.name}</h3>
+                <p>${contact.email}</p>
+            </div>
+        `;
+        contactList.appendChild(contactElement);
+    }
+}
+
+fetchContacts();
