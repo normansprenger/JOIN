f67766d26ec192ff0caec29ffe66894457f38f07
Norman <normansprenger@gmail.com>
2024-07-09 21:17:45 +0200

    update Database and SignUp

--- js/privacy_policy.js
+++ js/privacy_policy.js
@@ -1,3 +1,5 @@
+const BASE_URL = 'https://remotestoragejoin-d0140-default-rtdb.europe-west1.firebasedatabase.app/';
+
 async function initPrivacyPolicy() {
     await includeHTML(); // Sicherstellen, dass includeHTML() abgeschlossen ist
     if (currentUser == null) {
@@ -25,5 +27,12 @@ function hideDesktopContainer() {
 function backToLastPage() {
     console.log('Funktion fertig stellen. Merker für letzte Seite implementieren');
     open("../index.html", "_self");
+}
+
+async function loadData(path = "") {
+    let response = await fetch(BASE_URL + path + ".json");
+    let reponseToJSON = await response.json();
+    console.log(reponseToJSON);
+    document.getElementById('container').innerHTML = reponseToJSON['name'];
 }
 
--- js/signUp.js
+++ js/signUp.js
@@ -1,5 +1,9 @@
 let passwordFilled = false;
+let confirmPasswordFilled = false;
 let privacyPolicyAccepted = false;
+let users = [];
+const BASE_URL = "https://remotestoragejoin-d0140-default-rtdb.europe-west1.firebasedatabase.app"
+
 
 function changeCheckImg() {
     if (document.getElementById('checkedImg').style.display == 'none') {
@@ -15,18 +19,34 @@ function changeCheckImg() {
     }
 }
 
+
 function changePasswordIcon() {
-    if (!document.getElementById('signUpPassword').value == '') {
+    if (!document.getElementById('signUpPassword').value == '' && passwordFilled == false) {
         document.getElementById('passwordIcon').src = '../assets/img/visibility_off.svg';
         passwordFilled = true;
     }
     else if (document.getElementById('signUpPassword').value == '') {
         document.getElementById('passwordIcon').src = '../assets/img/lock.svg';
+        document.getElementById("signUpPassword").type = "password";
         passwordFilled = false;
     }
 }
 
-function toggleVisibilityIcon() {
+
+function changeConfirmPasswordIcon() {
+    if (!document.getElementById('signUpConfirmPassword').value == '' && confirmPasswordFilled == false) {
+        document.getElementById('confirmPasswordIcon').src = '../assets/img/visibility_off.svg';
+        confirmPasswordFilled = true;
+    }
+    else if (document.getElementById('signUpConfirmPassword').value == '') {
+        document.getElementById('confirmPasswordIcon').src = '../assets/img/lock.svg';
+        document.getElementById("signUpConfirmPassword").type = "password";
+        confirmPasswordFilled = false;
+    }
+}
+
+
+function toggleVisibilityPasswordIcon() {
     if (document.getElementById("signUpPassword").type === "password" && passwordFilled == true) {
         document.getElementById('passwordIcon').src = '../assets/img/visibility.svg';
         document.getElementById("signUpPassword").type = "text";
@@ -37,9 +57,21 @@ function toggleVisibilityIcon() {
     }
 }
 
-function signUp() {
-    console.log('registriert Baby');
+
+function toggleVisibilityConfirmPasswordIcon() {
+    if (document.getElementById("signUpConfirmPassword").type === "password" && confirmPasswordFilled == true) {
+        document.getElementById('confirmPasswordIcon').src = '../assets/img/visibility.svg';
+        document.getElementById("signUpConfirmPassword").type = "text";
+    }
+    else if (document.getElementById("signUpConfirmPassword").type === "text" && confirmPasswordFilled == true) {
+        document.getElementById('confirmPasswordIcon').src = '../assets/img/visibility_off.svg';
+        document.getElementById("signUpConfirmPassword").type = "password";
+    }
 }
+
+
+
+
 
 function validateEmail() {
     const emailElement = document.getElementById('email');
@@ -55,6 +87,7 @@ function validateEmail() {
     emailElement.setCustomValidity(errorMessage);
 }
 
+
 function enableDisableButton() {
     if ((privacyPolicyAccepted)
         && (!document.getElementById('signUpName').value == '')
@@ -63,5 +96,80 @@ function enableDisableButton() {
         && (!document.getElementById('signUpConfirmPassword').value == '')) { document.getElementById('signUpButton').disabled = false; }
     else { document.getElementById('signUpButton').disabled = true; }
 
+}
+
+function changeBordorColorName() {
+    document.getElementById('inputContainerName').style.border = "1px solid #29ABE2";
+}
+
+function originalBorderColorName() {
+    document.getElementById('inputContainerName').style.border = "1px solid #D1D1D1";
+}
+
+function changeBordorColorEmail() {
+    document.getElementById('inputContainerEmail').style.border = "1px solid #29ABE2";
+}
+
+function originalBorderColorEmail() {
+    document.getElementById('inputContainerEmail').style.border = "1px solid #D1D1D1";
+}
+
+function changeBordorColorPassword() {
+    document.getElementById('inputContainerPassword').style.border = "1px solid #29ABE2";
+}
+
+function originalBorderColorPassword() {
+    document.getElementById('inputContainerPassword').style.border = "1px solid #D1D1D1";
+}
+
+function changeBordorColorConfirmPassword() {
+    document.getElementById('inputContainerConfirmPassword').style.border = "1px solid #29ABE2";
+}
+
+function originalBorderColorConfirmPassword() {
+    document.getElementById('inputContainerConfirmPassword').style.border = "1px solid #D1D1D1";
+}
+
+function signUp(event) {
+    event.preventDefault()
+    passwordsEqual = false;
+    checkPasswords();
+    console.log('REGISTRATION started');
+    if (passwordsEqual == true) {
+        let name = document.getElementById('signUpName').value;
+        let email = document.getElementById('signUpEmail').value;
+        let password = document.getElementById('signUpName').value;
+        users.push({ "name": name, "email": email, "password": password })
+    }
+}
+
+function checkPasswords() {
+    if (document.getElementById('signUpPassword').value == document.getElementById('signUpConfirmPassword').value) {
+        document.getElementById('inputContainerConfirmPassword').style.border = "1px solid #D1D1D1";
+        document.getElementById('errorMessage').style.display = "none";
+        return passwordsEqual = true;
+    }
+    else {
+        document.getElementById('inputContainerConfirmPassword').style.border = "1px solid #FF8190";
+        document.getElementById('errorMessage').style.display = "block";
+    }
+}
+
+
+async function loadUsers(path = "/users") {
+    let response = await fetch(BASE_URL + path + ".json");
+    let reponseToJSON = await response.json();
+    users = reponseToJSON;
+};
+
+
+async function saveUsers(path = "/users") {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "PUT",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(users)
+    });
 }
 
