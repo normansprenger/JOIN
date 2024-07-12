f3f4bf07e7eb72e62ede1565b19c7717b84a7146
codelicious94 <simi.matter@gmail.com>
2024-07-12 02:40:17 +0200

    update contacts

--- js/contacts.js
+++ js/contacts.js
@@ -45,83 +45,85 @@ function displayContacts(data) {
 }
 
 function toggleContactView(id) {
-  const contact = contactsData[id];
-
-  if (!contact) {
-    console.error(`Contact with ID ${id} not found`);
-    return;
-  }
-
-  const contactItems = document.querySelectorAll(".contactItem");
-  contactItems.forEach((item) => {
-    item.classList.remove("selected");
-  });
-
-  const selectedContactElement = document.querySelector(
-    `.contactItem[onclick="toggleContactView(${id})"]`
-  );
-  if (selectedContactElement) {
-    selectedContactElement.classList.add("selected");
-  }
-
-  const contactDetailsSection = document.getElementById(
-    "contactDetailsSection"
-  );
-
-  const existingContactDetails = document.getElementById(
-    "dynamicContactDetails"
-  );
-  if (existingContactDetails) {
-    contactDetailsSection.removeChild(existingContactDetails);
-  }
-
-  const contactDetailsContainer = document.createElement("div");
-  contactDetailsContainer.id = "dynamicContactDetails";
-
-  contactDetailsContainer.innerHTML = /*HTML*/ `
-        <div class="contactDetailsHeader">
-            <div class="shortNameContactDetails" style="background-color: ${
-              contact.color
-            };">
-                <p class="initialsContactDetails">${contact.initials}</p>
-            </div>
-            <div class="ContactDetailsContainer">
-                <h2>${contact.name}</h2>
-                <div class="EditDeleteContainer">
-                    <div class="edit">
-                        <img src="../assets/img/edit.svg" alt="Edit">
-                        <p onclick="editContact(${id})">Edit</p>  
-                    </div>
-                    <div class="delete">
-                        <img src="../assets/img/delete.svg" alt="Delete">
-                        <p onclick="deleteContact(${id})">Delete</p>
-                    </div>
-                </div>
-            </div>
+    const contact = contactsData[id];
+  
+    if (!contact) {
+      console.error(`Contact with ID ${id} not found`);
+      return;
+    }
+  
+    const contactItems = document.querySelectorAll(".contactItem");
+    contactItems.forEach((item) => {
+      item.classList.remove("selected");
+    });
+  
+    const selectedContactElement = document.querySelector(
+      `.contactItem[onclick="toggleContactView(${id})"]`
+    );
+    if (selectedContactElement) {
+      selectedContactElement.classList.add("selected");
+    }
+  
+    const contactDetailsSection = document.getElementById(
+      "contactDetailsSection"
+    );
+  
+    const existingContactDetails = document.getElementById(
+      "dynamicContactDetails"
+    );
+    if (existingContactDetails) {
+      contactDetailsSection.removeChild(existingContactDetails);
+    }
+  
+    const contactDetailsContainer = document.createElement("div");
+    contactDetailsContainer.id = "dynamicContactDetails";
+  
+    contactDetailsContainer.innerHTML = /*HTML*/ `
+      <div class="contactDetailsHeader">
+        <div class="shortNameContactDetails" style="background-color: ${
+          contact.color
+        };">
+          <p class="initialsContactDetails">${contact.initials}</p>
         </div>
-        <div class="contactDetailsBodyContainer">
-            <h3>Contact Information</h3>
-                <div class="contactDetailsBodyEmailContainer">
-                    <p>Email:</p>
-                    <p class="contactDetailsBodyEmail">${contact.email}</p>
-                </div>
-                <div class="contactDetailsBodyPhoneContainer">
-                    <p>Phone:</p>
-                    <p class="contactDetailsBodyPhone">+${
-                      contact.phone || "N/A"
-                    }</p>
-                </div>
-            <img src="../assets/img/arrowLeft.svg" alt="Back" class="backButton" onclick="backToList()">
+        <div class="ContactDetailsContainer">
+          <h2>${contact.name}</h2>
+          <div class="EditDeleteContainer">
+            <div class="edit">
+              <img src="../assets/img/edit.svg" alt="Edit">
+              <p onclick="editContact(${id})">Edit</p>  
+            </div>
+            <div class="delete">
+              <img src="../assets/img/delete.svg" alt="Delete">
+              <p onclick="deleteContact(${id})">Delete</p>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="contactDetailsBodyContainer">
+        <h3>Contact Information</h3>
+        <div class="contactDetailsBodyEmailContainer">
+          <p>Email:</p>
+          <p class="contactDetailsBodyEmail">${contact.email}</p>
+        </div>
+        <div class="contactDetailsBodyPhoneContainer">
+          <p>Phone:</p>
+          <p class="contactDetailsBodyPhone">${contact.phone || "N/A"}</p>
+        </div>
+        <img src="../assets/img/arrowLeft.svg" alt="Back" class="backButton" onclick="backToList()">
+        <div class="ContactMenu" id="ContactMenu" onclick="openContactMenu(${id})">
+          <img src="../assets/img/more_vert.svg" alt="Menü">
+        </div>
+      </div>
     `;
-
-  contactDetailsSection.appendChild(contactDetailsContainer);
-
-  if (window.innerWidth <= 1000) {
-    document.querySelector(".listSection").style.display = "none";
-    contactDetailsSection.style.display = "block";
+  
+    contactDetailsSection.appendChild(contactDetailsContainer);
+  
+    if (window.innerWidth <= 1000) {
+      document.querySelector(".listSection").style.display = "none";
+      contactDetailsSection.style.display = "block";
+    }
   }
-}
+  
 
 function backToList() {
   document.querySelector(".listSection").style.display = "block";
@@ -170,7 +172,7 @@ function openAddContactPopUp() {
                     <label for="telNumber"></label>
                     <input class="CreateContactInput" type="tel" id="telNumber" pattern="^(\+[0-9\/ ]+|[0-9\/ ]+)$" placeholder="Phone" required>
                 </div>
-                <button class="AddContactButton">Create contact<img src="../assets/img/check.svg" alt="Check"></button>
+                <button class="AddContactButton" onclick="pushContacts()">Create contact<img src="../assets/img/check.svg" alt="Check"></button>
                 </form>
             </div>
         </div> 
@@ -194,23 +196,67 @@ async function fetchContacts() {
   }
 }
 
+function openContactMenu(id) {
+    const contact = contactsData[id];
+  
+    if (!contact) {
+      console.error(`Contact with ID ${id} not found`);
+      return;
+    }
+  
+    const popup = document.createElement("div");
+    popup.className = "contactMenuPopup";
+  
+    popup.innerHTML = /*HTML*/ `
+      <div class="contactMenuPopupContent">
+        <div class="editContact">
+            <img src="../assets/img/edit.svg" alt="Edit">
+            <p onclick="editContact(${id})">Edit</p>
+        </div>
+        <div class="deleteContact">
+            <img src="../assets/img/delete.svg" alt="Delete">
+            <p onclick="deleteContact(${id})">Delete</p>                
+        </div>
+      </div>
+    `;
+
+    document.body.appendChild(popup);
+}
+  
 fetchContacts();
 
-function pushContacts() {
-  let name = document.getElementById('signUpName').value;
-  let email = document.getElementById('signUpEmail').value;
-  let initials = getInitials(name);
-  let color = randomColor(userColors);
-  let id = new Date().getTime();
-  let phone = '';
-  let contact = {
+async function pushContacts() {
+    let name = document.getElementById('fullName').value;
+    let email = document.getElementById('mail').value;
+    let phone = document.getElementById('telNumber').value;
+    let initials = getInitials(name);
+    let color = randomColor(userColors);
+    let id = new Date().getTime();
+    let contact = {
       'color': `${color}`,
       'email': `${email}`,
       'id': `${id}`,
       'initials': `${initials}`,
       'name': `${name}`,
       'phone': `${phone}`
-  };
-  contacts.push(contact);
-  saveContacts(); // Speichern der neuen Kontakte in Firebase
+    };
+    
+    // Add to local contacts array
+    contacts.push(contact);
+  
+  }
+
+  function getInitials(name) {
+    // Trimmen des Namens und Aufteilen in Wörter
+    let words = name.trim().split(/\s+/);
+
+    // Wenn der Name nur aus einem Wort besteht, gebe die erste Initiale zurück
+    if (words.length === 1) {
+        return words[0].charAt(0).toUpperCase();
+    }
+
+    // Wenn der Name aus Vor- und Nachnamen besteht, gebe die Initialen zurück
+    let initials = words[0].charAt(0).toUpperCase() + words[1].charAt(0).toUpperCase();
+    return initials;
 }
+  
