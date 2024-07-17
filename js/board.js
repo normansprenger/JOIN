c2a1f547cb2d0591cabcbe70de00e9cbe0cd64fd
Norman <normansprenger@gmail.com>
2024-07-17 14:54:15 +0200

    update board

--- js/board.js
+++ js/board.js
@@ -22,14 +22,14 @@ function renderTodosBoard() {
         let taskId = toDos[i]['id'];
         document.getElementById('taskContainerContentTodo').innerHTML += /*html*/ `
         <div class="task" id="task${taskId}">
-                    <div class="taskCategory">${toDos[i]['category']}</div>
+                    <div class="taskCategory" id="category${taskId}">${toDos[i]['category']}</div>
                     <span class="taskTitle">${toDos[i]['title']}</span>
                     <span class="taskDescription">${toDos[i]['description']}</span>
-                            <div class="progressContainer">
+                            <div class="progressContainer" id="progressContainer${taskId}">
                                 <div class="progressBar">
                                     <div class="progress" id="progress${taskId}"></div>
                                 </div>
-                                <div class="progressText">0/2 Subtasks</div>
+                                <div class="progressText">0/<span id="progressText${taskId}">5</span> Subtasks</div>
                             </div>
                             <div class="taskBottom">
                                 <div class="assignedTo">
@@ -41,24 +41,34 @@ function renderTodosBoard() {
                             </div>
                         </div>
                 `;
-
-    }
-
-}
-
-function fillSubTasks(i, subTasksLength, taskId) {
-    if (subTasksLength > 0) {
-        calcProgressWidth(i, subTasksLength, taskId);
-        document.getElementById(`${toDos[i]['id']}`).innerHTML += /*html*/ `
-            <div class="progressContainer">
-                <div class="progressBar">
-                    <div class="progress"></div>
-                </div>
-                <div class="progressText">0/<span>${subTasksLength}</span> Subtasks</div>
-            </div>
-            `
+    changeCategoryColor(taskId, toDos[i]['category']);
+    changeProgressInfos(taskId, toDos[i]['subTasks']);
+    changeAssignedToUsers(taskId, toDos[i])
     }
 }
 
-function calcProgressWidth(i, subTasksLength, taskId) {
+
+function changeCategoryColor(taskId, category){
+    let color = '';
+    if(category === "User Story"){
+        color = 'userStoryColor'
+    } 
+    else if (category === 'Technical Tasks'){
+        color = 'technicalTaskColor'
+    }
+    document.getElementById(`category${taskId}`).classList.add(`${color}`);
 }
+
+
+function changeProgressInfos(taskId, subTasks){
+    if(subTasks == undefined){
+        document.getElementById(`progressContainer${taskId}`).classList.add('dnone')
+    } else {
+        let length = subTasks.length;
+        document.getElementById(`progressText${taskId}`).innerHTML = `${length}`;
+        let completedCount = subTasks.filter(subTask => subTask.completet).length;
+        let width = Math.round((completedCount / length) * 100);
+        document.getElementById(`progress${taskId}`).style.width = `${width}%`;
+    }
+}
+
