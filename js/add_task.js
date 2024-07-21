6571d6db731af02a79955e90abcab74fccf46ceb
codelicious94 <simi.matter@gmail.com>
2024-07-22 01:53:54 +0200

    update img and addtask

--- js/add_task.js
+++ js/add_task.js
@@ -1,3 +1,78 @@
+let assignetTo = document.getElementById("assignedTo");
+let category = document.getElementById("category");
+let priority;
+let subArray = [];
+let assignedContacts = [];
+
+async function initAddTask() {
+    changePriority(medium);
+    renderContactsInAddTasks();
+  }
+
+async function renderContactsInAddTasks() {
+    await loadContacts();
+
+    const selectElement = document.getElementById('assignedTo');
+    
+    const firstOption = selectElement.options[0];
+    selectElement.innerHTML = '';
+    selectElement.appendChild(firstOption);
+    
+    contacts.forEach(contact => {
+        const option = document.createElement('option');
+        option.value = contact.id; 
+        option.textContent = contact.name; 
+        selectElement.appendChild(option);
+    });
+}
+
+// Prio Button Color function //
+
+function addPrioButtonColor(prio, event) {
+    event.preventDefault();
+    let buttonUrgent = document.getElementById("buttonUrgent");
+    let buttonMedium = document.getElementById("buttonMedium");
+    let buttonLow = document.getElementById("buttonLow");
+    let imgUrgent = document.getElementById("buttonImg1");
+    let imgMedium = document.getElementById("buttonImg2");
+    let imgLow = document.getElementById("buttonImg3");
+    removeClasses(buttonUrgent, buttonMedium, buttonLow, imgUrgent, imgMedium, imgLow);
+    if (prio === "urgent") {
+        selectedButtonColor(buttonUrgent, imgUrgent, "backgroundColorRed", "urgent");
+    } else if (prio === "medium") {
+        selectedButtonColor(buttonMedium, imgMedium, "backgroundColorOrange", "medium");
+    } else if (prio === "low") {
+        selectedButtonColor(buttonLow, imgLow, "backgroundColorGreen", "low");
+    }
+}
+
+function removeClasses(buttonUrgent, buttonMedium, buttonLow, imgUrgent, imgMedium, imgLow) {
+    buttonUrgent.classList.remove("backgroundColorRed", "buttonPrioWhite");
+    buttonMedium.classList.remove("backgroundColorOrange", "buttonPrioWhite");
+    buttonLow.classList.remove("backgroundColorGreen", "buttonPrioWhite");
+    imgUrgent.src = "../assets/img/addTaskUrgentLogo.svg";
+    imgMedium.src = "../assets/img/addTaskMediumLogo.svg";
+    imgLow.src = "../assets/img/addTaskLowLogo.svg";
+}
+
+function selectedButtonColor(button, img, colorClass, priority) {
+    button.classList.add(colorClass, "buttonPrioWhite");
+    img.src = `../assets/img/addTask${capitalizeFirstLetter(priority)}LogoWhite.svg`;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function ButtonMediumInOrange() {
+    let buttonMedium = document.getElementById("buttonMedium");
+    let imgMedium = document.getElementById("buttonImg2");
+    buttonMedium.classList.add("backgroundColorOrange", "buttonPrioWhite");
+    imgMedium.src = "../assets/img/addTaskMediumLogoWhite.svg";
+}
+
+// Kalender min = Tagesdatum //
+
 document.addEventListener('DOMContentLoaded', function() {
     const dateInput = document.getElementById('dateInput');
     if (dateInput) {
